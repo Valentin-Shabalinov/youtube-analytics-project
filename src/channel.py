a9bbc63d3f8030a858234c3b6b3aedcbf7b50eab
a1 <4267817@mail.ru>
2023-08-10 00:05:29 +0300

    homework-2

--- src/channel.py
+++ src/channel.py
@@ -13,6 +13,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = self.youtube.channels().list(id=channel_id, part="snippet,statistics").execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.video_count = channel["items"][0]["statistics"]["viewCount"]
         
 
     def print_info(self) -> None:
@@ -23,3 +28,19 @@ class Channel:
             .execute()
         )
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @staticmethod
+    def get_service():
+        api_key: str = os.getenv('YT_API_KEY')
+        return build("youtube", "v3", developerKey=api_key)
+    
+    def to_json(self, file):
+        self.file = file
+        attrib_dict = {
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "video_count": self.video_count
+        }
+        with open(self.file, "w", encoding="utf-8") as f:
+            json.dump(attrib_dict, f, indent=2, ensure_ascii=False)
