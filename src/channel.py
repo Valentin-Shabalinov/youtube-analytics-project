7aa11b93fa45ada17741434d65b4fbb3f362b290
a1 <4267817@mail.ru>
2023-08-11 21:30:43 +0300

    homework-3

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Any
 
 from googleapiclient.discovery import build
 
@@ -7,18 +8,48 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    api_key: str = os.environ.get('YT_API_KEY')
+    api_key: str = os.environ.get("YT_API_KEY")
     youtube = build("youtube", "v3", developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        channel = self.youtube.channels().list(id=channel_id, part="snippet,statistics").execute()
+        channel = (
+            self.youtube.channels()
+            .list(id=channel_id, part="snippet,statistics")
+            .execute()
+        )
         self.title = channel["items"][0]["snippet"]["title"]
         self.description = channel["items"][0]["snippet"]["description"]
         self.url = f"https://www.youtube.com/channel/{self.channel_id}"
         self.video_count = channel["items"][0]["statistics"]["viewCount"]
-        
+        self.subscriber_count: int = channel["items"][0]["statistics"][
+            "subscriberCount"
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other: int):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other: int):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other: int):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __gt__(self, other: int):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __le__(self, other: int):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __ge__(self, other: int):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __eq__(self, other: int):
+        return int(self.subscriber_count) == int(other.subscriber_count)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -31,16 +62,16 @@ class Channel:
 
     @staticmethod
     def get_service():
-        api_key: str = os.getenv('YT_API_KEY')
+        api_key: str = os.getenv("YT_API_KEY")
         return build("youtube", "v3", developerKey=api_key)
-    
+
     def to_json(self, file):
         self.file = file
         attrib_dict = {
             "title": self.title,
             "description": self.description,
             "url": self.url,
-            "video_count": self.video_count
+            "video_count": self.video_count,
         }
         with open(self.file, "w", encoding="utf-8") as f:
             json.dump(attrib_dict, f, indent=2, ensure_ascii=False)
